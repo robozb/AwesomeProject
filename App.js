e8ddfe4c0468bac84974195a0d3e99feee5452cd
RBeci <rbelatamas@gmail.com>
2023-09-04 15:50:58 +0200

    create

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  function alma() {
+    let a=1
+    a=2
+    a=3
+    debugger
+    a=4
+    a=5
+    debugger
+    a=6
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!!</Text>
       <StatusBar style="auto" />
+      <Button title='alma' onPress={()=>{alma()}}></Button>
     </View>
   );
 }
